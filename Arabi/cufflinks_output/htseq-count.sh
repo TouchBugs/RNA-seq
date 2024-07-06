6d0fa808aeb4768cd82f7850ffecb3449a937435
TouchBugs <2196692208@qq.com>
2024-07-06 11:48:59 +0800

    整理

--- Arabi/cufflinks_output/htseq-count.sh
+++
@@ -1,7 +0,0 @@
-htseq-count -f bam -r pos -s no -t gene -i gene_id At-L0h_sorted.bam ../../reference/Arab/Arabidopsis_thaliana.TAIR10.59.gff3 > At-L0h_counts.txt &
-htseq-count -f bam -r pos -s no -t gene -i gene_id At-L2h_sorted.bam ../../reference/Arab/Arabidopsis_thaliana.TAIR10.59.gff3 > At-L2h_counts.txt &
-htseq-count -f bam -r pos -s no -t gene -i gene_id At-L24h_sorted.bam ../../reference/Arab/Arabidopsis_thaliana.TAIR10.59.gff3 > At-L24h_counts.txt &
-htseq-count -f bam -r pos -s no -t gene -i gene_id At-L168h_sorted.bam ../../reference/Arab/Arabidopsis_thaliana.TAIR10.59.gff3 > At-L168h_counts.txt &
-wait
-echo "FINISHED!!!"
-
--- Rice/cufflinks_output/htseq-count.sh
+++
@@ -1,7 +0,0 @@
-htseq-count -f bam -r pos -s no -t gene -i gene_id Os-L0h_sorted.bam ../../reference/Rice/Oryza_sativa.IRGSP-1.0.59.chr.gff3 > Os-L0h_counts.txt &
-htseq-count -f bam -r pos -s no -t gene -i gene_id Os-L2h_sorted.bam ../../reference/Rice/Oryza_sativa.IRGSP-1.0.59.chr.gff3 > Os-L2h_counts.txt &
-htseq-count -f bam -r pos -s no -t gene -i gene_id Os-L24h_sorted.bam ../../reference/Rice/Oryza_sativa.IRGSP-1.0.59.chr.gff3 > Os-L24h_counts.txt &
-htseq-count -f bam -r pos -s no -t gene -i gene_id Os-L168h_sorted.bam ../../reference/Rice/Oryza_sativa.IRGSP-1.0.59.chr.gff3 > Os-L168h_counts.txt &
-wait
-echo "FINISHED!!!"
-
